4feb71717c77c5b93872d20e91e0022d2bccea6c
SergiusLo <loboyko99911@gmail.com>
2023-01-11 00:09:28 +0200

    auth provider

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+
+const firebaseConfig = {
+   apiKey: "AIzaSyDAWXX40X5-_mEY0YQ2Vv6WAWOv4Q4fD_U",
+   authDomain: "next-app-auth-44089.firebaseapp.com",
+   projectId: "next-app-auth-44089",
+   storageBucket: "next-app-auth-44089.appspot.com",
+   messagingSenderId: "25141053151",
+   appId: "1:25141053151:web:fb90e9ae50792194e58b6a"
+ };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import { signIn, singOut, useSession } from "next-auth/react";
 
 function Header() {
   return (
@@ -28,7 +29,7 @@ function Header() {
         </div>
         {/* Right */}
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div className="link">
+          <div onClick={signIn} className="link">
             <p>Hello Serj Serjius</p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
