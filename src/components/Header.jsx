c9fc341be78ffe0361834e3bdb1303ca3685dfb8
SergiusLo <loboyko99911@gmail.com>
2023-01-11 23:05:51 +0200

    checkout page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,31 @@
 import Image from "next/image";
+import { useRouter } from 'next/router'
 import {
   MenuIcon,
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 import { signIn, singOut, useSession } from "next-auth/react";
+import Link from 'next/link'
 
 function Header() {
   const { data: session } = useSession();
+  const router = useRouter()
 
   return (
     <header>
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          {/* <Link href='/'> */}
           <Image
+          onClick={() => router.push('/')}
             src="/images/logo.png"
             width={150}
             height={40}
             objectFit="contain"
             className="cursor-pointer"
           />
+          {/* </Link> */}
         </div>
 
         <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
@@ -46,9 +52,10 @@ function Header() {
               0
             </span>
             <ShoppingCartIcon className="h-10" />
-            <p className="hidden md:inline font-extrabold md:text-sm mt-2">
-              Basket
-            </p>
+
+            <Link href='/checkout' >
+             <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p> 
+            </Link>
           </div>
         </div>
       </div>
