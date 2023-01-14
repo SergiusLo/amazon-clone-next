9b121e29b9d3a800ef2fc8d656d059df5a75cb88
SergiusLo <loboyko99911@gmail.com>
2023-01-14 02:42:51 +0200

    secret

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -10,7 +10,7 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.SECRET,
 };
 
 export default NextAuth(authOptions);
