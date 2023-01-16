81a01ddecf7d096519664ffed2578574a292bfae
SergiusLo <loboyko99911@gmail.com>
2023-01-16 21:05:53 +0200

    Update create-checkout-session.js

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -24,8 +24,8 @@ export default async (req, res) => {
     },
     line_items: transformedItems,
     mode: "payment",
-    success_url: `${process.env.HOST}/success`,
-    cancel_url: `${process.env.HOST}/checkout`,
+    success_url: `${process.env.NEXTAUTH_URL}/success`,
+    cancel_url: `${process.env.NEXTAUTH_URL}/checkout`,
     metadata: {
       email,
       images: JSON.stringify(items.map((item) => item.image)),
