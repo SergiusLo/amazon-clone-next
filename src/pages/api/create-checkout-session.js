92f02ae72b3395f7446c22512fdb5ea6ba42b4f5
SergiusLo <loboyko99911@gmail.com>
2023-01-16 21:18:00 +0200

    Update create-checkout-session.js

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -17,7 +17,7 @@ export default async (req, res) => {
   }));
 
   const session = await stripe.checkout.session.create({
-    payment_method_types: ["card"],
+    //  payment_method_types: ["card"],
     shipping_rates: ["shr_1MQxA3AEfILJ7IL6zva96fC6"],
     shipping_address_collection: {
       allowed_countries: ["GB", "US", "CA"],
