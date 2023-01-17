f9b638df155ee8ed2911b8688048b80505be0999
SergiusLo <loboyko99911@gmail.com>
2023-01-17 22:45:40 +0200

    webhook implementing

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -2,38 +2,35 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
   const { items, email } = req.body;
+
   const transformedItems = items.map((item) => ({
-    // if quantities are bundled, this needs to change.
     quantity: 1,
-
     price_data: {
-      currency: "usd",
-
+      currency: "gbp",
       unit_amount: item.price * 100,
-
       product_data: {
-        name: item.title,
         description: item.description,
+        name: item.title,
         images: [item.image],
       },
     },
   }));
 
   const session = await stripe.checkout.sessions.create({
+    //  payment_method_types: ["card"],
+    //  shipping_rates: ["shr_1MQxA3AEfILJ7IL6zva96fC6"],
+    shipping_address_collection: {
+      allowed_countries: ["GB", "US", "CA"],
+    },
     line_items: transformedItems,
-
     mode: "payment",
-
     success_url: `${process.env.HOST}/success`,
-
     cancel_url: `${process.env.HOST}/checkout`,
-
     metadata: {
       email,
-
       images: JSON.stringify(items.map((item) => item.image)),
     },
   });
-
   res.status(200).json({ id: session.id });
+  // res.status(err.statusCode || 500 || 400).json(err.message);
 };
---
+++ src/pages/success.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function success(props) {
+   return (
+      <div className='bg-gray-100 h-screen'>
+         
+      </div>
+   );
+}
+
+export default success;
