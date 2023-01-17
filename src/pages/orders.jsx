2b03b970ed3711a60aa3b135898f82dc5c6750a2
SergiusLo <loboyko99911@gmail.com>
2023-01-17 23:33:43 +0200

    order page 1/2

--- src/pages/orders.jsx
+++ src/pages/orders.jsx
@@ -1,8 +1,11 @@
 import Header from "../components/Header";
-import {useSession} from 'next-auth/client'
+import { useSession, getSession } from "next-auth/react";
+import db from "../../firebase";
+import moment from "moment";
 
-function Orders(props) {
-   const [session] = useSession()
+function Orders({ orders }) {
+  const { data: session, status } = useSession();
+  console.log(orders);
   return (
     <div>
       <Header />
@@ -10,10 +13,53 @@ function Orders(props) {
         <h1 className="text-3xl border-b mb-2 pb-1 border-yellow-400">
           Your Orders
         </h1>
-
+        {session ? <h2>x Orders</h2> : <h2>Sign in to see your orders</h2>}
+        <div className="mt-5 space-y-4 "></div>
       </main>
     </div>
   );
 }
 
 export default Orders;
+
+export async function getServerSideProps(context) {
+  const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+  // Get the users logged in credentials...
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      props: {},
+    };
+  }
+  // Firebase db
+  const stripeOrders = await db
+    .collection("users")
+    .doc(session.user.email)
+    .collection("orders")
+    .orderBy("timestamp", "desc")
+    .get();
+
+  //Stripe orders
+  const orders = await Promise.all(
+    stripeOrders.docs.map(async (order) => ({
+      id: order.id,
+      amount: order.data().amount,
+      amountShipping: order.data().amount_shipping,
+      images: order.data().images,
+      timestamp: moment(order.data().timestamp.toDate()).unix(),
+      items: (
+        await stripe.checkout.sessions.listLineItems(order.id, {
+          limit: 100,
+        })
+      ).data,
+    }))
+  );
+
+  return {
+    props: {
+      orders,
+    },
+  };
+}
