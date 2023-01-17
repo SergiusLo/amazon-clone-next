b4b5c0583f9389407078b524a0942ec0f934af2f
SergiusLo <loboyko99911@gmail.com>
2023-01-17 17:44:17 +0200

    try fix 500

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -2,35 +2,38 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
   const { items, email } = req.body;
-
   const transformedItems = items.map((item) => ({
+    // if quantities are bundled, this needs to change.
     quantity: 1,
+
     price_data: {
-      currency: "gbp",
+      currency: "usd",
+
       unit_amount: item.price * 100,
+
       product_data: {
-        description: item.description,
         name: item.title,
+        description: item.description,
         images: [item.image],
       },
     },
   }));
 
   const session = await stripe.checkout.sessions.create({
-    //  payment_method_types: ["card"],
-    //  shipping_rates: ["shr_1MQxA3AEfILJ7IL6zva96fC6"],
-    shipping_address_collection: {
-      allowed_countries: ["GB", "US", "CA"],
-    },
     line_items: transformedItems,
+
     mode: "payment",
+
     success_url: `${process.env.NEXTAUTH_URL}/success`,
+
     cancel_url: `${process.env.NEXTAUTH_URL}/checkout`,
+
     metadata: {
       email,
+
       images: JSON.stringify(items.map((item) => item.image)),
     },
   });
+
   res.status(200).json({ id: session.id });
-  res.status(err.statusCode || 500).json(err.message);
 };
